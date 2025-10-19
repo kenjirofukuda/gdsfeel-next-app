56c393d6293b0da47932034b4b6bbd287fb57bce
kenjirofukuda <kenjirofukuda@gmail.com>
2025-10-19 14:22:21 +0900

    geo.ts add types

--- src/geo.ts
+++ src/geo.ts
@@ -1,245 +1,282 @@
-const GEO = {};
 
-GEO.EPS = 1e-8;
+namespace GEO {
 
-GEO.sameValue = function(v1, v2, eps = GEO.EPS) {
-  return Math.abs(v1 - v2) < eps;
+  type ViewportArgProc = (vp: Viewport) => void;
+  type MatrixFunction = () => createjs.Matrix2D;
+  type WheelDirection = 1 | -1;
+  type Matrix2D = createjs.Matrix2D;
+
+  class Point extends createjs.Point {
+    constructor(x: number, y: number) {
+      super(x, y);
+    }
+
+    equals(other: Point): boolean {
+      return this.x === other.x && this.y === other.y;
+    }
+
+    minus(other: Point): Point {
+      return new Point(this.x - other.x, this.y - other.y);
+    }
+  }
+
+  type dimensionIndex = 0 | 1 | 2; // x, y, z
+  type PointLike = Point | Array<dimensionIndex>;
+
+  class Rectangle extends createjs.Rectangle {
+    center(): Point {
+      return new Point(
+        this.x + this.width / 2.0,
+        this.y + this.height / 2.0);
+    };
+
+    pointArray(): Point[] {
+      const points = [];
+      points.push(new Point(this.x, this.y));
+      points.push(new Point(this.x, this.y + this.height));
+      points.push(new Point(this.x + this.width, this.y + this.height));
+      points.push(new Point(this.x + this.width, this.y));
+      return points;
+    };
+  }
+
+
+  const EPS = 1e-8;
+
+  function sameValue(v1: number, v2: number, eps = EPS) {
+    return Math.abs(v1 - v2) < eps;
+  }
+
+
+  class Viewport {
+    private width: number;
+    private height: number;
+    private scale: number;
+    private centerX: number;
+    private centerY: number;
+    private portCenterX: number;
+    private portCenterY: number;
+    private _transform: Matrix2D | null;
+    private _invertTransform: Matrix2D | null;
+    private _basicTransform: Matrix2D | null;
+    private transformStack: Matrix2D[];
+    private portDamageFunction: ViewportArgProc | null;
+    private transformFunction: MatrixFunction | null;
+
+    constructor(width: number, height: number) {
+      this.width = width;
+      this.height = height;
+      this.scale = 1.0;
+      this.centerX = 0.0;
+      this.centerY = 0.0;
+      this._resetPortCenter();
+      this.portCenterX = width / 2.0;
+      this.portCenterY = height / 2.0;
+      this._transform = null;
+      this._invertTransform = null;
+      this._basicTransform = null;
+      this.transformStack = [];
+      this.portDamageFunction = null;
+      this.transformFunction = null;
+    }
+
+    get transformDepth(): number {
+      return this.transformStack.length;
+    }
+
+    wheelZoom(h: number, v: number, x: number, y: number, direction: WheelDirection): void {
+      this.portCenterX = h;
+      this.portCenterY = this.height - v;
+      this.centerX = x;
+      this.centerY = y;
+      this.scale = this.scale * (1.0 + (0.125 * direction));
+      this._damageTransform();
+    }
+
+    zoomDouble(): void {
+      this.setScale(this.scale * 2.0);
+    }
+
+    zoomHalf(): void {
+      this.setScale(this.scale * 0.5);
+    }
+
+    setScale(scale: number): void {
+      this.scale = scale;
+      this._damageTransform();
+    }
+
+    setCenter(x: number, y: number) {
+      this.centerX = x;
+      this.centerY = y;
+      this._damageTransform();
+    }
+
+    setSize(width: number, height: number) {
+      this.width = width;
+      this.height = height;
+      this._damageTransform();
+    }
+
+    reset(): void {
+      this.scale = 1.0;
+      this.centerX = 0;
+      this.centerY = 0;
+      this._damageTransform();
+    }
+
+    transform(): Matrix2D {
+      if (typeof (this.transformFunction) === 'function') {
+        return this.transformFunction();
+      }
+      if (this._transform === null) {
+        this._transform = this._lookupTransform();
+      }
+      return this._transform;
+    }
+
+    invertTransform(): Matrix2D {
+      if (this._invertTransform === null) {
+        this._invertTransform = this.transform().clone().invert();
+      }
+      return this._invertTransform;
+    }
+
+    deviceToWorld(h: number, v: number): Point {
+      //    const result = MakePoint(0, 0);
+      return this.invertTransform().transformPoint(h, v /*, result */) as Point;
+    }
+
+    worldToDevice(x: number, y: number) {
+      //  const result = GEO.MakePoint();
+      return this.transform().transformPoint(x, y);
+    }
+
+    pushTransform(transform: Matrix2D) {
+      this.transformStack.push(transform);
+      this._damageTransform();
+    }
+
+    popTransform(): Matrix2D | null | any {
+      if (this.transformStack.length === 0) {
+        return new createjs.Matrix2D();
+      }
+      const result: any = this.transformStack.pop();
+      this._damageTransform();
+      return result;
+    }
+
+    _lookupTransform(): Matrix2D {
+      const newTransform = new createjs.Matrix2D();
+      newTransform.prependMatrix(this.basicTransform());
+      this.transformStack.map(function (item) {
+        newTransform.prependMatrix(item);
+      });
+      return newTransform;
+    }
+
+    basicTransform(): Matrix2D {
+      if (this._basicTransform === null) {
+        this._basicTransform = this._lookupBasicTransform();
+      }
+      return this._basicTransform;
+    }
+
+    _lookupBasicTransform(): Matrix2D {
+      const tx = new createjs.Matrix2D();
+      tx.translate(this.portCenterX, this.height - this.portCenterY);
+      tx.scale(this.scale, -this.scale);
+      tx.translate(-this.centerX, -this.centerY);
+      return tx;
+    }
+
+    _fittingRatio(width: number, height: number): number {
+      const margin = 20;
+      const xRatio = (this.width - margin) / width;
+      const yRatio = (this.height - margin) / height;
+      return Math.min(xRatio, yRatio);
+    }
+
+    setRectangle(r: Rectangle) {
+      this._resetPortCenter();
+      const center = r.center();
+      this.setCenter(center.x, center.y);
+      this.setScale(this._fittingRatio(r.width, r.height));
+    }
+
+    _resetPortCenter(): void {
+      this.portCenterX = this.width / 2.0;
+      this.portCenterY = this.height / 2.0;
+    }
+
+    _damageTransform(): void {
+      this._basicTransform = null;
+      this._transform = null;
+      this._invertTransform = null;
+      if (this.portDamageFunction !== null) {
+        const self = this;
+        this.portDamageFunction(self);
+      }
+    }
+  } // Viewport
+
+
+  function point_x(obj: PointLike): number {
+    if ('x' in obj) {
+      return obj['x'];
+    }
+    else {
+      return obj[0];
+    }
+  };
+
+  function point_y(obj: PointLike): number {
+    if ('y' in obj) {
+      return obj['y'];
+    }
+    else {
+      return obj[1];
+    }
+  };
+
+
+  function calcExtentBounds(points: any[]) {
+    let minX = Number.MAX_VALUE;
+    let maxX = Number.MIN_VALUE;
+    let minY = Number.MAX_VALUE;
+    let maxY = Number.MIN_VALUE;
+
+    points.forEach(function (p) {
+      minX = Math.min(point_x(p), minX);
+      maxX = Math.max(point_x(p), maxX);
+      minY = Math.min(point_y(p), minY);
+      maxY = Math.max(point_y(p), maxY);
+    });
+    return MakeRect(
+      minX, minY, Math.abs(maxX - minX), Math.abs(maxY - minY));
+  };
+
+
+  function MakeRect(x: number, y: number, width: number, height: number): createjs.Rectangle {
+    return new createjs.Rectangle(x, y, width, height);
+  }
+
+
+  function MakePoint(x: number, y: number): createjs.Point {
+    return new createjs.Point(x, y);
+  }
+
+} // namespace GEO
+
+interface Number {
+  roundDigits(dig: number): number;
 }
 
-
-GEO.Viewport = class {
-  constructor(width, height) {
-    this.width = width;
-    this.height = height;
-    this.scale = 1.0;
-    this.centerX = 0.0;
-    this.centerY = 0.0;
-    this._resetPortCenter();
-    this.portCenterY = height / 2.0;
-    this._transform = null;
-    this._invertTransform = null;
-    this._basicTransform = null;
-    this.transformStack = [];
-    this.portDamageFunction = null;
-    this.transformFunction = null;
-  }
-
-  get transformDepth() {
-    return this.transformStack.length;
-  }
-  
-  wheelZoom(h, v, x, y, direction) {
-    this.portCenterX = h;
-    this.portCenterY = this.height - v;
-    this.centerX = x;
-    this.centerY = y;
-    this.scale = this.scale * (1.0 + (0.125 * direction));
-    this._damageTransform();
-  }
-
-  zoomDouble() {
-    this.setScale(this.scale * 2.0);
-  }
-
-  zoomHalf() {
-    this.setScale(this.scale * 0.5);
-  }
-
-  setScale(scale) {
-    this.scale = scale;
-    this._damageTransform();
-  }
-
-  setCenter(x, y) {
-    this.centerX = x;
-    this.centerY = y;
-    this._damageTransform();
-  }
-
-  setSize(width, height) {
-    this.width = width;
-    this.height = height;
-    this._damageTransform();
-  }
-
-  reset() {
-    this.scale = 1.0;
-    this.centerX = 0;
-    this.centerY = 0;
-    this._damageTransform();
-  }
-
-  transform() {
-    if (typeof(this.transformFunction) === 'function') {
-      return this.transformFunction();
-    }
-    if (this._transform === null) {
-      this._transform = this._lookupTransform();
-    }
-    return this._transform;
-  }
-
-  invertTransform() {
-    if (this._invertTransform === null) {
-      this._invertTransform = this.transform().clone().invert();
-    }
-    return this._invertTransform;
-  }
-
-  deviceToWorld(h, v) {
-    const result = GEO.MakePoint();
-    return this.invertTransform().transformPoint(h, v, result);
-  }
-
-  worldToDevice(x, y) {
-    const result = GEO.MakePoint();
-    return this.transform().transformPoint(x, y);
-  }
-
-  pushTransform(transform) {
-    this.transformStack.push(transform);
-    this._damageTransform();
-  }
-
-  popTransform() {
-    if (this.transformStack.length === 0) {
-      return null;
-    }
-    const result = this.transformStack.pop();
-    this._damageTransform();
-    return result;
-  }
-
-  _lookupTransform() {
-    const newTransform = new createjs.Matrix2D();
-    newTransform.prependMatrix(this.basicTransform());
-    this.transformStack.map(function (item) {
-      newTransform.prependMatrix(item);
-    });
-    return newTransform;
-  }
-
-  basicTransform() {
-    if (this._basicTransform === null) {
-      this._basicTransform = this._lookupBasicTransform();
-    }
-    return this._basicTransform;
-  }
-
-  _lookupBasicTransform() {
-    const tx = new createjs.Matrix2D();
-    tx.translate(this.portCenterX, this.height - this.portCenterY);
-    tx.scale(this.scale, -this.scale);
-    tx.translate(-this.centerX, -this.centerY);
-    return tx;
-  }
-
-  _fittingRatio(width, height) {
-    const margin = 20;
-    const xRatio = (this.width - margin) / width;
-    const yRatio = (this.height - margin) / height;
-    return Math.min(xRatio, yRatio);
-  }
-
-  setRectangle(r) {
-    this._resetPortCenter();
-    const center = r.center();
-    this.setCenter(center.x, center.y);
-    this.setScale(this._fittingRatio(r.width, r.height));
-  }
-
-  _resetPortCenter() {
-    this.portCenterX = this.width / 2.0;
-    this.portCenterY = this.height / 2.0;
-  }
-
-  _damageTransform() {
-    this._basicTransform = null;
-    this._transform = null;
-    this._invertTransform = null;
-    if (this.portDamageFunction !== null) {
-      const self = this;
-      this.portDamageFunction(self);
-    }
-  }  
-};
-
-
-GEO.point_x = function (obj){
-  if ('x' in obj) {
-    return obj['x'];
-  }
-  else {
-    return obj[0];
-  }
-};
-
-GEO.point_y = function (obj){
-  if ('y' in obj) {
-    return obj['y'];
-  }
-  else {
-    return obj[1];
-  }
-};
-
-
-GEO.calcExtentBounds = function (points) {
-  let minX = Number.MAX_VALUE;
-  let maxX = Number.MIN_VALUE;
-  let minY = Number.MAX_VALUE;
-  let maxY = Number.MIN_VALUE;
-
-  points.forEach(function (p) {
-    minX = Math.min(GEO.point_x(p), minX);
-    maxX = Math.max(GEO.point_x(p), maxX);
-    minY = Math.min(GEO.point_y(p), minY);
-    maxY = Math.max(GEO.point_y(p), maxY);
-  });
-  return GEO.MakeRect(
-          minX, minY, Math.abs(maxX - minX), Math.abs(maxY - minY));
-};
-
-
-GEO.MakeRect = function (x, y, width, height) {
-  return new createjs.Rectangle(x, y, width, height);
-};
-
-GEO.MakePoint = function (x, y) {
-  return new createjs.Point(x, y);
-};
-
-createjs.Point.prototype.equals = function (other) {
-  return this.x === other.x && this.y === other.y;
-};
-
-
-createjs.Point.prototype.minus = function (other) {
-  return new createjs.Point(this.x - other.x, this.y - other.y);
-};
-
-
-createjs.Rectangle.prototype.center = function () {
-  return new createjs.Point(
-          this.x + this.width / 2.0,
-          this.y + this.height / 2.0);
-};
-
-createjs.Rectangle.prototype.pointArray = function () {
-  const points = [];
-  points.push(new createjs.Point(this.x, this.y));
-  points.push(new createjs.Point(this.x, this.y + this.height));
-  points.push(new createjs.Point(this.x + this.width, this.y + this.height));
-  points.push(new createjs.Point(this.x + this.width, this.y));
-  return points;
-};
-
-
-function floatConvertSyncer(num: number, dig) {
+function floatConvertSyncer(num: number, dig: number): number {
   const p = Math.pow(10, dig);
   return Math.round(num * p) / p;
 }
 
-Number.prototype.roundDigits = function (dig) {
-  return floatConvertSyncer(this, dig);
+Number.prototype.roundDigits = function (dig: number): number {
+  return floatConvertSyncer(this as number, dig);
 };
